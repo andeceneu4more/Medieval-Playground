edcd0edfde448ad68ad722f30c81e8f9232ca0f1
andeceneu4more <andeceneu4more@github.com>
2019-04-09 16:57:28 +0300

    added ignore collision between players

---
+++ Assets/Scripts/DragonControler.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class DragonControler : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float jump;
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("HorizontalDragon");
+        animator.SetFloat("Horizontal", moveHorizontal);
+
+        if (moveHorizontal < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (moveHorizontal > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        rb.velocity = new Vector2(speed * moveHorizontal, rb.velocity.y);
+
+
+        /*float moveVertical = Input.GetAxis("VerticalDragon");
+
+        rb.velocity = new Vector2(rb.velocity.x, speed * moveVertical);*/
+
+        /*
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+        */
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+
+
+    }
+}
