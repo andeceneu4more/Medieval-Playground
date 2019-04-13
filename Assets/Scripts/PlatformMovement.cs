657f2acc20f1fdd98c27b2a74d5a8dc265241b0f
Gidea Andrei <andrei.gadea@yahoo.com>
2019-04-13 15:46:50 +0300

    platform added, character's collider update

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    private Vector3 posLeverOFF;
+
+    private Vector3 posLeverON;
+
+    private Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform transformLeverOFF;
+
+    [SerializeField]
+    private Transform transformLeverON;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posLeverOFF = transformLeverOFF.localPosition;
+        posLeverON = transformLeverON.localPosition;
+        nextPos = posLeverON;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transformLeverOFF.localPosition = Vector3.MoveTowards(transformLeverOFF.localPosition, nextPos, speed * Time.deltaTime);
+        if (Vector3.Distance(transformLeverOFF.localPosition, nextPos) <= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+
+    private void ChangeDestination()
+    {
+        nextPos = nextPos != posLeverOFF ? posLeverOFF : posLeverON;
+    }
+}
