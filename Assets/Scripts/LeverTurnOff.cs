1359187c87a829c9a99b0cc980f3f23bbd779eb9
andeceneu4more <andeceneu4more@github.com>
2019-05-07 13:59:26 +0300

    added flipping lever

---
+++ Assets/Scripts/LeverTurnOff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTurnOff : MonoBehaviour
+{
+    [SerializeField] private static bool visible = true;
+    [SerializeField] private SpriteRenderer sr;
+
+    void Start()
+    {
+        if (visible == true)
+            sr.enabled = true;
+        else
+            if (visible == false)
+                sr.enabled = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (visible == false)
+        {
+            visible = true;
+            sr.enabled = false;
+        }
+        else
+            if (visible == true)
+            {
+                visible = false;
+                sr.enabled = true;
+            }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnMouseDown();
+    }
+}
---
+++ Assets/Scripts/LeverTurnOn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTurnOn : MonoBehaviour
+{
+    [SerializeField] private bool visible = false;
+    [SerializeField] private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr.enabled = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (visible == false)
+        {
+            visible = true;
+            sr.enabled = false;
+        }
+        else
+            if (visible == true)
+        {
+            visible = false;
+            sr.enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnMouseDown();
+    }
+}
